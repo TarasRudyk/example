3ba6e4677d67815ad7dbdeade99c2ef226c72e63
Taras Bilozir <taras.bilozir@keenethics.com>
2016-10-05 11:32:50 +0300

    i18n added

--- client/client.jsx
+++ client/client.jsx
@@ -1,5 +1,14 @@
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Karma from './karma.jsx';
+import { TAPi18n, TAPi18next } from 'meteor/tap:i18n';
+
+class Karma extends Component {
+    render() {
+        return (
+            <div>{TAPi18n.__('karma')}</div>
+        );
+    }
+}
 
 Meteor.startup(() => {
   ReactDOM.render(<Karma />, document.getElementById('app'));
