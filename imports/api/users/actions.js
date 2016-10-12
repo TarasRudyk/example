eb83355d740e3d613ea7c13d8e14e1fddd3764de
TarasRudyk <rudyktaras@gmail.com>
2016-10-12 19:21:25 +0300

    user-info refactoring

--- imports/api/users/actions.js
+++ imports/api/users/actions.js
@@ -37,25 +37,44 @@ export const register = (email, password, isUsernameTaken) => {
   check(password, String);
   check(isUsernameTaken, Match.Optional(String));
 
-  if (email && password) {
-    Accounts.createUser({
-      email: email,
-      password: password,
-      profile: {
-        avatar: `https://www.gravatar.com/avatar/${md5(email)}`
-      }
-    }, (err) => {
-      if (err) {
-        return err.reason;
-      } else {
-        FlowRouter.go('/user-info');
-      }
-    });
-  } else if (!isUsernameTaken) {
-      return TAPi18n.__('Please fill all fields.');
-  } else if (isUsernameTaken) {
-      return TAPi18n.__('Username is already taken.');
+  Accounts.createUser({
+    email: email,
+    password: password,
+    profile: {
+      avatar: `https://www.gravatar.com/avatar/${md5(email)}`
+    }
+  }, (err) => {
+    if (err) {
+      return err.reason;
+    } else {
+      FlowRouter.go('/user-info');
+    }
+  });
+  if (!isUsernameTaken) {
+    return TAPi18n.__('Please fill all fields.')
   }
+  if (isUsernameTaken) {
+    return TAPi18n.__('Username is already taken.')
+  }
+}
+
+export const saveData = (fullName, username) => {
+
+  if (!fullName || !username) {
+    return TAPi18n.__('Please fill all fields.')
+  }
+
+  Meteor.call('editUserData', {
+    fullName: fullName,
+    username: username
+  }, (err) => {
+    if (err) {
+      console.error(error);
+      return TAPi18n.__('Error occured.');
+    } else {
+      FlowRouter.go('/');
+    }
+  });
 }
 
 export const logout = () => {
--- imports/ui/pages/user-info.jsx
+++ imports/ui/pages/user-info.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { saveData } from '/imports/api/users/actions.js';
 
 export default class UserInfo extends React.Component {
   constructor(props) {
@@ -10,31 +11,28 @@ export default class UserInfo extends React.Component {
   }
   saveData(event) {
     event.preventDefault();
+    const fullName = this.refs['full-name'].value.trim(),
+          username = this.refs.username.value.trim();
 
-    if (this.refs['full-name'].value.trim() && this.refs.username.value.trim()) {
-      Meteor.call('editUserData', {
-        fullName: this.refs['full-name'].value.trim(),
-        username: this.refs.username.value.trim()
-      }, (error) => {
-        if (!error) {
-          FlowRouter.go('/');
-        } else {
-          console.error(error);
-          this.setState({message: 'Error occured.'});
-        }
-      });
-    } else {
-      this.setState({message: 'Please fill all fields.'});
-    }
+    saveData(fullName, username);
   }
   render() {
     return (
       <div className="page-content">
         <h2>User info</h2>
         <form onSubmit={this.saveData}>
-          <img src={this.props.userData && this.props.userData.profile && this.props.userData.profile.avatar ? this.props.userData.profile.avatar : ''}></img>
-          <input type="text" ref="full-name" placeholder="Enter new full name" defaultValue={this.props.userData && this.props.userData.profile && this.props.userData.profile.fullName ? this.props.userData.profile.fullName : ''} />
-          <input type="text" ref="username" placeholder="Enter new username" defaultValue={this.props.userData && this.props.userData.profile && this.props.userData.profile.username ? this.props.userData.profile.username : ''} />
+          <img
+            src={this.props.userData && this.props.userData.profile &&
+              this.props.userData.profile.avatar ?
+              this.props.userData.profile.avatar : ''}></img>
+          <input type="text" ref="full-name" placeholder="Enter new full name"
+             defaultValue={this.props.userData && this.props.userData.profile
+               && this.props.userData.profile.fullName ?
+               this.props.userData.profile.fullName : ''} />
+          <input type="text" ref="username" placeholder="Enter new username"
+            defaultValue={this.props.userData && this.props.userData.profile
+              && this.props.userData.profile.username ?
+              this.props.userData.profile.username : ''} />
           <input type="submit" />
         </form>
         <label>{this.state.message}</label>
