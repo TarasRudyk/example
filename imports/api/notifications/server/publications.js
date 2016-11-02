c7e525382bb156017ded0fb790ea95a5ebf34d76
IgVol <igor.voloshenko@keenethics.com>
2016-11-02 14:59:19 +0200

    Publication of notifications in sidebar

--- imports/api/notifications/server/publications.js
+++ imports/api/notifications/server/publications.js
@@ -7,3 +7,12 @@ import { Notifications } from '../notifications';
 Meteor.publish('notifications', function () {
   return Notifications.find({ recipientId: this.userId, read: false });
 });
+
+Meteor.publish('notifications-new', function () {
+  return Notifications.find({ recipientId: this.userId, read: false });
+});
+
+Meteor.publish('notifications-read', function (limit = 0, skip = 0) {
+  return Notifications.find({ recipientId: this.userId, read: true },
+        { limit, skip });
+});
--- imports/ui/containers/components/side-content/notifications.js
+++ imports/ui/containers/components/side-content/notifications.js
@@ -5,9 +5,18 @@ import { Notifications } from '/imports/api/notifications/notifications';
 import NotificationsList from '/imports/ui/components/side-content/notifications';
 
 export default createContainer(() => {
-  const notificationsHandle = Meteor.subscribe('notifications');
-  const notifications = notificationsHandle.ready() ? Notifications.find().fetch() : [];
-
+  let notificationsHandle = Meteor.subscribe('notifications-new');
+  let notifications = notificationsHandle.ready()
+    ? Notifications.find({}, { sort: { creationDate: -1 } }).fetch()
+    : [];
+  const unreadCount = Notifications.find({ read: false }).count();
+  if (unreadCount < 15 && notificationsHandle.ready()) {
+    notificationsHandle = Meteor.subscribe('notifications-read', 15 - unreadCount, 0);
+    notifications = notificationsHandle.ready()
+      ? Notifications.find({}, { sort: { creationDate: -1 } }).fetch()
+      : [];
+  }
+  console.log(Notifications.find().fetch());
   return {
     notifications
   };
