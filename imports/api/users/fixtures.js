d4831012d2f9c5aa198c770155ca1b3d2acfc0bd
IgVol <igor.voloshenko@keenethics.com>
2016-11-07 10:50:19 +0200

    Colors added

--- imports/api/users/fixtures.js
+++ imports/api/users/fixtures.js
@@ -4,6 +4,8 @@ import md5 from 'js-md5';
 
 import { Accounts } from 'meteor/accounts-base';
 
+import { Colors } from '/imports/api/colors/colors';
+
 const result = [];
 
 if (!Meteor.users.find().count()) {
@@ -24,4 +26,22 @@ if (!Meteor.users.find().count()) {
       }
     }
   });
+}
+
+if (Colors.find().count() === 0) {
+  const colors = [
+    '#1da9fc', '#fc363b', '#FFFC54', '#F77A52', '#9D397E',
+    '#BEDB39', '#79BD8F', '#77fc75', '#1F8A70', '#911146',
+    '#F29C9C', '#D8CAA8', '#ED8C2B', '#68FFDA', '#5E5A59',
+    '#FF6F69', '#FFE11A', '#CC4452', '#77C4D3', '#7E8AA2'
+  ];
+
+  for (let i = 0; i < 20; i += 1) {
+    for (let j = 0; j < 20; j += 1) {
+      Colors.insert({ gradient: { start: colors[i], stop: colors[j], direction: 'horizontal' } });
+      Colors.insert({ gradient: { start: colors[i], stop: colors[j], direction: 'vertical' } });
+    }
+  }
+  Colors.insert({ gradient: { start: 'hey', stop: 'hey', direction: 'hey' } });
+  Colors.insert({ gradient: { start: 'heyd', stop: 'hdey', direction: 'hefdy' } });
 }
