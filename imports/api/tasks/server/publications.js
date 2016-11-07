05ad482ffacbfd07bda7786103fa8559ba2641c3
kuksik <nazarkukarkin@gmail.com>
2016-11-07 10:04:50 +0200

    added create task method & action

--- imports/api/tasks/server/publications.js
+++ imports/api/tasks/server/publications.js
@@ -2,14 +2,7 @@ import { Meteor } from 'meteor/meteor';
 // import { check } from 'meteor/check';
 
 import { Tasks } from '../tasks';
-//
-// Meteor.publish('tasks', function () {
-//   return Tasks.find();
-// });
-Meteor.publish('projectssa', function () {
+
+Meteor.publish('tasks.byProject', function () {
   return Tasks.find({ ownerId: this.userId, active: true });
 });
-
-// Meteor.publish('tasks.byProject', function () {
-//   return Tasks.find();
-// });
