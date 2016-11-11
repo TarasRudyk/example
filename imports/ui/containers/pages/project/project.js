4e5ec088b80bcb166c1779ca058e24e9c71df234
Arsen <arsen@Arsens-MacBook-Air.local>
2016-11-11 16:29:59 +0200

    don't update project component when update people list

--- imports/ui/containers/pages/project/project.js
+++ imports/ui/containers/pages/project/project.js
@@ -2,23 +2,20 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Projects } from '/imports/api/projects/projects';
-import { Invitations } from '/imports/api/invitations/invitations';
 
 import SingleProject from '/imports/ui/pages/project/project';
 
 export default createContainer(({ id }) => {
   const projectHandle = Meteor.subscribe('project', id);
   const project = projectHandle.ready() ? Projects.findOne() : {};
-  const invitationsHandle = Meteor.subscribe('invitationsByProject', id);
-  const invitations = invitationsHandle.ready() ? Invitations.find(
-    { 'project.id': id, replied: false },
-    { skip: 0, limit: 25 }
-  ).fetch() : [];
   const isOwner = Meteor.userId() === project.ownerId;
-
+  const { _id, name, ownerName, description, ownerId } = project;
   return {
-    project,
-    invitations,
+    id: _id,
+    name,
+    ownerName,
+    description,
+    ownerId,
     isOwner
   };
 }, SingleProject);
