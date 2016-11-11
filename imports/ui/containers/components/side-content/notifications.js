a77536250d813ceb0e44d7e443176c766bea4324
TarasRudyk <rudyktaras@gmail.com>
2016-11-11 15:53:26 +0200

    notif in 15 last items only

--- imports/ui/containers/components/side-content/notifications.js
+++ imports/ui/containers/components/side-content/notifications.js
@@ -12,22 +12,20 @@ export default createContainer(() => {
     ? Notifications.find({ read: false }, { sort: { creationDate: -1 } }).fetch()
     : [];
 
-  const unreadCount = Notifications.find({ read: false }).count();
+  const count = Notifications.find().count();
 
-  if (unreadCount < 15 && notificationsHandle.ready()) {
-    notificationsHandle = Meteor.subscribe('notifications-read', 15 - unreadCount, 0);
-    notifications = notificationsHandle.ready()
+
+  notificationsHandle = Meteor.subscribe('notifications-read', 15 - count, 0);
+  notifications = notificationsHandle.ready()
       ? notifications.concat(Notifications.find({ read: true },
-        { sort: { creationDate: -1 }, limit: 15 - unreadCount }).fetch())
+        { sort: { creationDate: -1 }, limit: 15 - count }).fetch())
       : notifications;
-  }
+
 
   const invitationsHandle = Meteor.subscribe('invitations');
   const invitations = invitationsHandle.ready()
     ? Invitations.find({ 'user.id': Meteor.userId(), replied: false }, { sort: { creationDate: -1 } }).fetch()
     : [];
-
-  const count = unreadCount + invitations.length;
 
   return {
     notifications,
