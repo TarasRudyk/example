c8f3ab4a8cf12077dcedd4d1e3f2293f64371f05
Arsen <arsen@Arsens-MacBook-Air.local>
2016-11-15 11:57:16 +0200

    show projects when user assigned

--- imports/ui/containers/pages/projects/list.js
+++ imports/ui/containers/pages/projects/list.js
@@ -6,7 +6,8 @@ import ProjectsList from '/imports/ui/pages/projects/list';
 export default createContainer((data) => {
   const skip = ((parseInt((data.page), 10) * 7) - 7);
   const projectsHandle = Meteor.subscribe('projects');
-  const projects = projectsHandle.ready() ? Projects.find({}, { skip, limit: 7 }).fetch() : [];
+  const projects = projectsHandle.ready() ? Projects.find({},
+    { sort: { ownerId: this.userId }, skip, limit: 7 }).fetch() : [];
   const projectsCount = projectsHandle.ready() ? Projects.find().count() : 0;
   return {
     projects, projectsCount, loaded: projectsHandle.ready(), page: parseInt((data.page), 10)
