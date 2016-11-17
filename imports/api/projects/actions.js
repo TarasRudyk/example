be8d31798ca5e6d42378a0865c19a91c7ba765fd
yataras <taras.yaskiv@gmail.com>
2016-11-17 14:34:10 +0200

    Change new project validation

--- imports/api/projects/actions.js
+++ imports/api/projects/actions.js
@@ -1,18 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
-import { TAPi18n } from 'meteor/tap:i18n';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import formatValidation from 'string-format-validation';
 import { addNotice } from '/imports/api/notices/actions';
 
 export const createProject = (name, description) => {
   check(name, String);
   check(description, String);
-
-  if (!formatValidation.validate({ min: 3, max: 25 }, name)) {
-    addNotice(TAPi18n.__('Project name is required'));
-    return false;
-  }
 
   return Meteor.call('project.create', { name, description }, (err, res) => {
     if (err) {
