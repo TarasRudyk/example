679dd8b81da26ddcdff8a55f58f70a320dc86d6a
TarasRudyk <rudyktaras@gmail.com>
2016-11-18 14:01:02 +0200

    color exception fixed

--- imports/ui/pages/projects/item.jsx
+++ imports/ui/pages/projects/item.jsx
@@ -10,24 +10,26 @@ export default class ProjectItem extends React.Component {
   render() {
     const { _id, name, ownerName } = this.props.project;
     const userProject = this.props.userProjects.find(elem => elem.projectId === _id);
-
-    return (
-      <div className="list-item">
-        <div className="project-item">
-          <div className="project-item-color">
-            <div className="ring" style={{ background: `repeating-linear-gradient(${userProject.color.gradient.direction}, ${userProject.color.gradient.start}, ${userProject.color.gradient.stop}` }} />
-          </div>
-          <div className="project-item-information">
-            <div className="project-item-title"><a href={`/project/${_id}`}>{name}</a></div>
-            <div className="project-item-subtitle">Owner: <a href="/">{ownerName}</a></div>
-          </div>
-          <div className="project-item-statistics">
-            <div className="project-item-stat-block">5 <span>people</span></div>
-            <div className="project-item-stat-block">28 <span>tasks</span></div>
+    if (userProject) {
+      return (
+        <div className="list-item">
+          <div className="project-item">
+            <div className="project-item-color">
+              <div className="ring" style={{ background: `repeating-linear-gradient(${userProject.color.gradient.direction}, ${userProject.color.gradient.start}, ${userProject.color.gradient.stop}` }} />
+            </div>
+            <div className="project-item-information">
+              <div className="project-item-title"><a href={`/project/${_id}`}>{name}</a></div>
+              <div className="project-item-subtitle">Owner: <a href="/">{ownerName}</a></div>
+            </div>
+            <div className="project-item-statistics">
+              <div className="project-item-stat-block">5 <span>people</span></div>
+              <div className="project-item-stat-block">28 <span>tasks</span></div>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    };
+    return null;
   }
 }
 
