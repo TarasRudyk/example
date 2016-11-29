8dfc6d41f42710829bd7c4704ca66481396224b3
yataras <taras.yaskiv@gmail.com>
2016-11-29 17:33:27 +0200

    Check if parameter for subscribtion is not empty

--- imports/ui/containers/pages/project/tabs/tasks/reassign-task.js
+++ imports/ui/containers/pages/project/tabs/tasks/reassign-task.js
@@ -5,6 +5,8 @@ import { Projects } from '/imports/api/projects/projects';
 import ReassignTask from '/imports/ui/pages/project/tabs/tasks/reassign-task';
 
 export default createContainer(({ task }) => {
+  if (!task.projectId) return {};
+
   const projectHandle = Meteor.subscribe('project', task.projectId);
   const project = projectHandle.ready() ? Projects.findOne() : {};
 
