81d7c778df054485e0fb6da1969e9ff088010edf
Taras Rudyk <rudyktaras@yahoo.com>
2016-11-29 20:49:29 +0200

    filter for sidebar

--- imports/ui/components/side-content/tasks.jsx
+++ imports/ui/components/side-content/tasks.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import { Draggable } from 'react-drag-and-drop';
+import { Meteor } from 'meteor/meteor';
 import TaskItem from './items/task';
 
 export default class Notifications extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedProj: '',
+      isAssigned: false
+    };
 
     this.getTasks = this.getTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleCheckbox = this.toggleCheckbox.bind(this);
   }
   getTasks() {
+    const { selectedProj, isAssigned } = this.state;
     if (this.props.tasks.length > 0) {
       return (
         <div className="list">
           {
-            this.props.tasks.map(t => (
+            this.props.tasks.filter((t) => {
+              if (selectedProj && isAssigned) {
+                return t.projectId === selectedProj && t.assignedAt === Meteor.userId();
+              }
+              if (!selectedProj && isAssigned) {
+                return t.assignedAt === Meteor.userId();
+              }
+              if (selectedProj && !isAssigned) {
+                return t.projectId === selectedProj;
+              }
+              return t;
+            }).map(t => (
               <Draggable key={t._id} type="_id" data={t._id}>
-                <TaskItem key={t._id} task={t} />
+                <TaskItem key={t._id} task={t} filter={selectedProj} isAssigned={isAssigned} />
               </Draggable>
             ))
           }
@@ -26,13 +44,38 @@ export default class Notifications extends React.Component {
 
     return null;
   }
+  handleChange(e) {
+    this.setState({
+      selectedProj: e.target.value
+    });
+  }
+  toggleCheckbox(e) {
+    this.setState({
+      isAssigned: e.target.checked
+    });
+  }
   render() {
+    const { userProjects } = this.props;
     return (
       <div className="page-side-content-wrapper">
         <div className="page-side-content-inner">
           <div className="page-side-content-panel">
             <div className="page-side-content-panel-inner">
               <div className="page-side-content-lists">
+                <select onChange={this.handleChange}>
+                  <option value="" default>All projects</option>
+                  {
+                    userProjects.map(item =>
+                      <option key={item.id} value={item.id}>{item.name}</option>)
+                  }
+                </select>
+                <input
+                  type="checkbox"
+                  id="toggleCheckbox"
+                  onChange={this.toggleCheckbox}
+                  checked={this.state.isAssigned}
+                />
+                <label htmlFor="toggleCheckbox">Assigned tasks</label>
                 {this.getTasks()}
                 {!this.props.tasks.length ? (
                   <div className="no-notifications">
@@ -51,5 +94,6 @@ export default class Notifications extends React.Component {
 }
 
 Notifications.propTypes = {
-  tasks: React.PropTypes.arrayOf(React.PropTypes.object)
+  tasks: React.PropTypes.arrayOf(React.PropTypes.object),
+  userProjects: React.PropTypes.arrayOf(React.PropTypes.object)
 };
--- imports/ui/containers/components/side-content/tasks.js
+++ imports/ui/containers/components/side-content/tasks.js
@@ -9,11 +9,20 @@ import TasksList from '/imports/ui/components/side-content/tasks';
 export default createContainer(() => {
   const userId = Meteor.userId();
   const userProjectIds = Projects.find({ 'users.id': userId }).map(project => project._id);
+  const userProjects = Projects.find({ 'users.id': userId }).map((project) => {
+    const projects = {
+      name: project.name,
+      id: project._id
+    };
+    return projects;
+  });
   const tasksHandle = Meteor.subscribe('tasks.byUserProjects', userProjectIds);
 
   const assignedAtTasks = tasksHandle.ready() ?
     Tasks.find({
-      projectId: { $in: userProjectIds }, assignedAt: userId, isAccepted: { $in: [null, false] }
+      projectId: { $in: userProjectIds },
+      assignedAt: userId,
+      isAccepted: { $in: [null, false] }
     },
       { sort: [['estimate', 'asc'], ['creationDate', 'desc']] }).fetch() : [];
 
@@ -27,6 +36,7 @@ export default createContainer(() => {
   const tasks = assignedAtTasks.concat(notAssignedAtTasks);
 
   return {
-    tasks
+    tasks,
+    userProjects
   };
 }, TasksList);
